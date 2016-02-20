1e20263ff7340179ec79a010b5ee5a85ccdb9998
Nisheeth Kashyap <nisheeth.k.kashyap@gmail.com>
2016-02-20 22:46:03 +0000

    added all fs async methods

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ class FileSystem {
         this.fs = require('fs');
     }
 
+    execute() {
+        var args = Array.from(arguments);
+        return (new Promise((resolve, reject) => {
+            args.push((error, arg1, arg2) => {
+                if (error) return reject(error);
+                resolve({args: [arg1, arg2]});
+            });
+
+            this.fs[args.shift()].apply(this.fs, args);
+        }));
+    }
+
     get F_OK() {
         return this.fs.F_OK;
     }
@@ -32,13 +44,129 @@ class FileSystem {
     }
 
     access(path, mode) {
-        return (new Promise((resolve, reject) => {
-            this.fs
-                .access(path, mode, (error) => {
-                    if (error) return reject(error);
-                    resolve();
-                });
-        }));
+        return this.execute('access', path, mode);
+    }
+
+    appendFile(file, data, options) {
+        return this.execute('appendFile', file, data, options);
+    }
+
+    chmod(path, mode) {
+        return this.execute('chmod', path, mode);
+    }
+
+    chown(path, uid, gid) {
+        return this.execute('chown', path, uid, gid);
+    }
+
+    close(fd) {
+        return this.execute('close', fd);
+    }
+
+    fchmod(fd, mode) {
+        return this.execute('fchmod', fd, mode);
+    }
+
+    fchown(fd, uid, gid) {
+        return this.execute('fchown', fd, uid, gid);
+    }
+
+    fstat(fd) {
+        return this.execute('fstat', fd);
+    }
+
+    fsync(fd) {
+        return this.execute('fsync', fd);
+    }
+
+    ftruncate(fd, len) {
+        return this.execute('ftruncate', fd, len);
+    }
+
+    futimes(fd, atime, mtime) {
+        return this.execute('futimes', fd, atime, mtime);
+    }
+
+    lchmod(path, mode) {
+        return this.execute('lchmod', path, mode);
+    }
+
+    lchown(path, uid, gid) {
+        return this.execute('lchown', path, uid, gid);
+    }
+
+    link(src, dst) {
+        return this.execute('link', src, dst);
+    }
+
+    lstat(path) {
+        return this.execute('lstat', path);
+    }
+
+    mkdir(path, mode) {
+        return this.execute('mkdir', path, mode);
+    }
+
+    open(path, flags, mode) {
+        return this.execute('open', path, flags, mode);
+    }
+
+    read(fd, buffer, offset, length, position) {
+        return this.execute('read', fd, buffer, offset, length, position);
+    }
+
+    readdir(path) {
+        return this.execute('readdir', path);
+    }
+
+    readFile(file, options) {
+        return this.execute('readFile', file, options);
+    }
+
+    readlink(path) {
+        return this.execute('readlink', path);
+    }
+
+    realpath(path, cache) {
+        return this.execute('realpath', path, cache);
+    }
+
+    rename(oldPath, newPath) {
+        return this.execute('rename', oldPath, newPath);
+    }
+
+    rmdir(path) {
+        return this.execute('rmdir', path);
+    }
+
+    stat(path) {
+        return this.execute('stat', path);
+    }
+
+    symlink(target, path, type) {
+        return this.execute('symlink', target, path, type);
+    }
+
+    truncate(path, len) {
+        return this.execute('truncate', path, len);
+    }
+
+    unlink(path) {
+        return this.execute('unlink', path);
+    }
+
+    utimes(path, atime, mtime) {
+        return this.execute('utimes', path, atime, mtime);
+    }
+
+    write(fd, buffer, offset, length, position) {
+        //fs.write(fd, buffer, offset, length[, position], callback)
+        //fs.write(fd, data[, position[, encoding]], callback)
+        return this.execute('write', fd, buffer, offset, length, position);
+    }
+
+    writeFile(file, data, options) {
+        return this.execute('writeFile', file, data, options);
     }
 }
 
--- test.js
+++ test.js
@@ -10,20 +10,109 @@ require('mocha');
 describe('fs-es6-promise', () => {
 
     describe('access()', () => {
-        it('should execute command with correct arguments', (done) => {
-            fs.access('/etc/passwd', fs.F_OK)
+        it('should resolve promise', (done) => {
+            fs.access('./index.js', fs.F_OK)
                 .then(()=> {
                     done();
                 });
         });
-        it('should fail to execute command ', (done) => {
-            fs.access('/etc/test', fs.X_OK)
+        it('should reject promise ', (done) => {
+            fs.access('./index-missing.js', fs.X_OK)
                 .catch((error) => {
+                    assert.equal(error.errno, -2);
                     assert.equal(error.code, 'ENOENT');
                     assert.equal(error.syscall, 'access');
-                    assert.equal(error.path, '/etc/test');
+                    assert.equal(error.path, './index-missing.js');
                     done();
                 });
         });
     });
+
+    describe('appendFile()', ()=> {
+        it('should resolve promise', (done) => {
+            fs.appendFile('./.test.txt', 'Hello World\n', {flag: 'a'})
+                .then(()=> {
+                    done();
+                });
+        });
+        it('should reject promise ', (done) => {
+            fs.appendFile('./.test.txt', 'Hello World\n', {flag: 'ax'})
+                .catch((error) => {
+                    assert.equal(error.errno, -17);
+                    assert.equal(error.code, 'EEXIST');
+                    assert.equal(error.syscall, 'open');
+                    assert.equal(error.path, './.test.txt');
+                    done();
+                });
+        });
+    });
+
+    describe('chmod()', ()=> {
+        it('should resolve promise', (done) => {
+            fs.chmod('./LICENSE', fs.F_OK)
+                .then(()=> {
+                    done();
+                });
+        });
+        it('should reject promise ', (done) => {
+            fs.chmod('./index-missing.js', fs.F_OK)
+                .catch((error) => {
+                    assert.equal(error.errno, -2);
+                    assert.equal(error.code, 'ENOENT');
+                    assert.equal(error.syscall, 'chmod');
+                    assert.equal(error.path, './index-missing.js');
+                    done();
+                });
+        });
+    });
+
+    describe('chown()', ()=> {
+        it('should resolve promise', (done) => {
+            fs.chown('./LICENSE')
+                .then(()=> {
+                    done();
+                });
+        });
+        it('should reject promise ', (done) => {
+            fs.chown('./index-missing.js')
+                .catch((error) => {
+                    assert.equal(error.errno, -2);
+                    assert.equal(error.code, 'ENOENT');
+                    assert.equal(error.syscall, 'chmod');
+                    assert.equal(error.path, './index-missing.js');
+                    done();
+                });
+        });
+    });
+
+
+    //fs.close(fd, callback)
+    //fs.fchmod(fd, mode, callback)
+    //fs.fchown(fd, uid, gid, callback)
+    //fs.fstat(fd, callback)
+    //fs.fsync(fd, callback)
+    //fs.ftruncate(fd, len, callback)
+    //fs.futimes(fd, atime, mtime, callback)
+
+    //fs.lchmod(path, mode, callback)
+    //fs.lchown(path, uid, gid, callback)
+    //fs.link(srcpath, dstpath, callback)
+    //fs.lstat(path, callback)
+    //fs.mkdir(path[, mode], callback)
+    //fs.open(path, flags[, mode], callback)
+    //fs.read(fd, buffer, offset, length, position, callback)
+    //fs.readdir(path, callback)
+    //fs.readFile(file[, options], callback)
+    //fs.readlink(path, callback)
+    //fs.realpath(path[, cache], callback)
+    //fs.rename(oldPath, newPath, callback)
+    //fs.rmdir(path, callback)
+    //fs.stat(path, callback)
+    //fs.symlink(target, path[, type], callback)
+    //fs.truncate(path, len, callback)
+    //fs.unlink(path, callback)
+    //fs.utimes(path, atime, mtime, callback)
+    //fs.write(fd, buffer, offset, length[, position], callback)
+    //fs.write(fd, data[, position[, encoding]], callback)
+    //fs.writeFile(file, data[, options], callback)
 });
